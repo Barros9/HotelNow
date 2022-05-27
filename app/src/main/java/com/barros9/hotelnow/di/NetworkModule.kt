eb6debcb02b3ea47d52eb52107c5e3e91102b626
Barros9 <giu.barreca@gmail.com>
2022-05-27 09:46:44 +0200

    Added logging and header interceptor

--- app/src/main/java/com/barros9/hotelnow/di/NetworkModule.kt
+++ app/src/main/java/com/barros9/hotelnow/di/NetworkModule.kt
@@ -1,14 +1,11 @@
 package com.barros9.hotelnow.di
 
+import com.barros9.hotelnow.data.network.NetworkService
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import io.ktor.client.HttpClient
-import io.ktor.client.engine.android.Android
-import io.ktor.client.features.json.JsonFeature
-import io.ktor.client.features.json.serializer.KotlinxSerializer
-import kotlinx.serialization.json.Json
 import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
@@ -18,20 +15,7 @@ class NetworkModule {
     @Singleton
     @Provides
     fun provideHttpClient(): HttpClient {
-        return HttpClient(Android) {
-            engine {
-                connectTimeout = 30_000
-                socketTimeout = 30_000
-            }
-            install(JsonFeature) {
-                serializer = KotlinxSerializer(
-                    Json {
-                        ignoreUnknownKeys = true
-                        isLenient = true
-                    }
-                )
-            }
-        }
+        return NetworkService.createHttpClient()
     }
 
 }
